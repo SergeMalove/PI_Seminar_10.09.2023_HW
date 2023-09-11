173b5799f816aa8ef08b7a56d09a9b079ee56ebe
Serg <serge-malov@yandex.ru>
2023-09-11 16:42:14 +0300

    First commit

---
+++ Task_1/Program.cs
@@ -0,0 +1,77 @@
+﻿// Задайте двумерный массив. Напишите программу, которая упорядочит по убыванию элементы каждой строки двумерного массива.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// В итоге получается вот такой массив:
+// 7 4 2 1
+// 9 5 3 2
+// 8 4 4 2
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+void SortRowsArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)    // Этот цикл бежит по столбцам
+    {
+        for (int j = 0; j < array.GetLength(1) - 1; j++)   // Два следующих цикла бегут по строке сортируя её (усовершенствованный пузырёк)
+        {
+            bool flag = true;
+            int max = j;
+            for (int k = 0; k < array.GetLength(1) - 1 - j; k++)
+            {
+                if (array[i, k] < array[i, k + 1])
+                {
+                    (array[i, k], array[i, k + 1]) = (array[i, k + 1], array[i, k]);
+                    flag = false;
+                }
+                if (array[i, k] > array[i, max])
+                    max = k;
+            }
+            if (flag)
+                break;
+            if (max > j)
+                (array[i, j], array[i, max]) = (array[i, max], array[i, j]);
+        }
+    }
+}
+
+int rows = InputNum("Введите количество строк массива: ");
+int columns = InputNum("Введите количество столбцов массива: ");
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+int[,] array = Create2DArray(rows, columns);
+Fill2DArray(array, minValue, maxValue);
+Console.WriteLine("\nИсходный массив:\n");
+Print2DArray(array);
+Console.WriteLine("\nИзменённый массив:\n");
+SortRowsArray(array);
+Print2DArray(array);
+Console.WriteLine();
---
+++ Task_2/Program.cs
@@ -0,0 +1,92 @@
+﻿// Задайте прямоугольный двумерный массив. Напишите программу, которая будет находить строку с наименьшей суммой элементов.
+// Например, задан массив:
+// 1 4 7 2
+// 5 9 2 3
+// 8 4 2 4
+// 5 2 6 7
+// Программа считает сумму элементов в каждой строке и выдаёт номер строки с наименьшей суммой элементов: 1 строка
+
+int InputNum(string message)
+{
+    Console.Write(message);
+    return int.Parse(Console.ReadLine()!);
+}
+
+int[,] Create2DArray(int rows, int columns)
+{
+    return new int[rows, columns];
+}
+
+void Fill2DArray(int[,] array, int minValue, int maxValue)
+{
+    Random rnd = new Random();
+    for (int i = 0; i < array.GetLength(0); i++)
+        for (int j = 0; j < array.GetLength(1); j++)
+            array[i, j] = rnd.Next(minValue, maxValue + 1);
+}
+
+void Print2DArray(int[,] array)
+{
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+            Console.Write($"{array[i, j]}\t");
+        Console.WriteLine();
+    }
+}
+
+// Решил, что метод будет возвращать массив, в который будут записаны суммы элементов всех строк
+// Таким образом, если вдруг сумма двух или более строк будут минимальными, будут выведены все
+
+int[] FindMinSumRow(int[,] array)
+{
+    // + 1 тут чтобы в последнюю ячейку массива записать найденную минимальную сумму
+    int[] rowsSums = new int[array.GetLength(0) + 1];
+    int minRowSum = 0;
+
+    for (int i = 0; i < array.GetLength(0); i++)
+    {
+        int rowSum = 0;
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            rowSum += array[i, j];
+        }
+
+        rowsSums[i] = rowSum;
+
+        if (rowSum < minRowSum || i == 0)
+        {
+            minRowSum = rowSum;
+        }
+    }
+
+    rowsSums[array.GetLength(0)] = minRowSum;
+
+    return rowsSums;
+}
+
+void PrintMinSumsRows(int[] array)
+{
+    int minSum = array[array.Length - 1];
+    Console.WriteLine($"В данном массиве нижеследующие строки имеют минимальную сумму всех элементов равную {minSum}:");
+
+    for (int i = 0; i < array.Length - 1; i++)
+    {
+        if (array[i] == minSum)
+        {
+            Console.WriteLine($"{i + 1} Строка");
+        }
+    }
+}
+
+int rows = InputNum("Введите количество строк массива: ");
+int columns = InputNum("Введите количество столбцов массива: ");
+int minValue = InputNum("Введите минимальное значение элемента: ");
+int maxValue = InputNum("Введите максимальное значение элемента: ");
+int[,] array = Create2DArray(rows, columns);
+Fill2DArray(array, minValue, maxValue);
+Console.WriteLine("\nИсходный массив:\n");
+Print2DArray(array);
+Console.WriteLine();
+PrintMinSumsRows(FindMinSumRow(array));
+Console.WriteLine();
