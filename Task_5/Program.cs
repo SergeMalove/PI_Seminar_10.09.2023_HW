b64e106ece48f78f59b10551889d068508181925
Serg <serge-malov@yandex.ru>
2023-09-13 16:24:51 +0300

    Task_5 print changed

--- Task_5/Program.cs
+++ Task_5/Program.cs
@@ -89,10 +89,19 @@ void Fill2DArray(int[,] array, int turn)
 
 void Print2DArray(int[,] array)
 {
+    int length = array.GetLength(0);
+    int digits = 0;
+    while (length != 0)
+    {
+        length /= 10;
+        digits++;
+    }
     for (int i = 0; i < array.GetLength(0); i++)
     {
         for (int j = 0; j < array.GetLength(1); j++)
-            Console.Write($"{array[i, j]}\t");
+        {
+            Console.Write($"{array[i, j]:d2}\t");
+        }
         Console.WriteLine();
     }
 }
